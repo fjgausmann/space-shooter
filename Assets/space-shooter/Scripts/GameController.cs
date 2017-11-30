49eacaeb9c597a288d7f48a25618b016106e2871
Finn Justus Gausmann <finn.gausmann@r132-03-w10.SCHULE-ISERV>
2017-11-30 14:40:46 +0100

    .

--- Assets/space-shooter/Scripts/GameController.cs
+++ Assets/space-shooter/Scripts/GameController.cs
@@ -10,9 +10,34 @@ public class GameController : MonoBehaviour
 	public float startWait;
 	public float waveWait;
 
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+
+	private bool gameOver;
+	private bool restart;
+	private int score;
+
 	void Start ()
 	{
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		UpdateScore ();
 		StartCoroutine (SpawnWaves ());
+	}
+
+	void Update ()
+	{
+		if (restart)
+		{
+			if (Input.GetKeyDown (KeyCode.R))
+			{
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
 	}
 
 	IEnumerator SpawnWaves ()
@@ -28,6 +53,28 @@ public class GameController : MonoBehaviour
 				yield return new WaitForSeconds (spawnWait);
 			}
 			yield return new WaitForSeconds (waveWait);
+			if (gameOver)
+			{
+				restartText.text = "Press 'R' for Restart";
+				restart = true;
+				break;
+			}
 		}
+	}
+
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: " + score;
+	}
+	public void GameOver ()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver = true;
 	}
 }
